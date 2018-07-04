38b9d6d4af4e28b1b3350ad5329da4ad5e02b43f
Alex Drozd <drozdster@gmail.com>
2018-07-03 23:21:30 -0500

    Adds options menu template

--- Project Boost/Assets/Scripts/MainMenu.cs
+++ Project Boost/Assets/Scripts/MainMenu.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour {
 
     public SceneLoader sceneLoader;
+    public Canvas mainMenu;
+    public GameObject mainMenuRocket;
 
-    // Use this for initialization
+    public Canvas optionsMenu;
+    public GameObject optionsMenuRocket1;
+    public GameObject optionsMenuRocket2;
+    
     void Start () {
-        
+        EnableMainMenu();
+        DisableOptions();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     public void Play()
     {
@@ -23,12 +24,47 @@ public class MainMenu : MonoBehaviour {
 
     public void Options()
     {
-        Debug.Log("Options pressed...");
+        EnableOptions();
+        DisableMainMenu();
+    }
+
+    public void Back()
+    {
+        DisableOptions();
+        EnableMainMenu();
     }
 
     public void Quit()
     {
         Debug.Log("Exiting...");
         Application.Quit();
+    }
+
+    private void DisableOptions()
+    {
+        optionsMenu.enabled = false;
+        optionsMenuRocket1.SetActive(false);
+        optionsMenuRocket2.SetActive(false);
+    }
+
+    private void EnableOptions()
+    {
+        optionsMenu.enabled = true;
+        optionsMenuRocket1.SetActive(true);
+        optionsMenuRocket2.SetActive(true);
+    }
+
+    private void DisableMainMenu()
+    {
+        mainMenu.enabled = false;
+        mainMenuRocket.SetActive(false);
+        mainMenuRocket.SetActive(false);
+    }
+
+    private void EnableMainMenu()
+    {
+        mainMenu.enabled = true;
+        mainMenuRocket.SetActive(true);
+        mainMenuRocket.SetActive(true);
     }
 }
