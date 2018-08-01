c6d32f9f9b07063d5d16508836cd5ee29d653ffb
Alex Drozd <drozdster@gmail.com>
2018-08-01 00:27:38 -0500

    adds quality cycle button

---
+++ Snow Lander/Assets/Scripts/QualityTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QualityTracker : MonoBehaviour {
+
+    [HideInInspector] static public int currentQual = 2;
+
+    private void Awake()
+    {
+        int numOfTrackers = FindObjectsOfType<GameObject>().Length;
+
+        if (numOfTrackers > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
--- Snow Lander/Assets/Scripts/UI Scripts/OptionsMenu.cs
+++ Snow Lander/Assets/Scripts/UI Scripts/OptionsMenu.cs
@@ -7,9 +7,14 @@ using UnityEngine.EventSystems;
 public class OptionsMenu : MonoBehaviour {
 
     public Text difficultyDisplayText;
+    public Text qualityDisplayText;
 
     private string easy = "Difficulty: Easy";
     private string hard = "Difficulty: Hard";
+
+    private string low = "Quality: Low";
+    private string medium = "Quality: Medium";
+    private string high = "Quality: High";
 
     private void Start()
     {
@@ -20,6 +25,12 @@ public class OptionsMenu : MonoBehaviour {
     {
         DifficultyTracker.isEasy = !DifficultyTracker.isEasy;
         SetDifficultyText();
+    }
+
+    public void CycleQuality()
+    {
+        QualityTracker.currentQual++;
+        SetQualityText();
     }
 
     public void SetDifficultyText()
@@ -37,5 +48,30 @@ public class OptionsMenu : MonoBehaviour {
         {
             difficultyDisplayText.text = hard;
         }
+    }
+
+    public void SetQualityText()
+    {
+        if (qualityDisplayText == null)
+        {
+            return;
+        }
+
+        int currentNormalizedQaul = QualityTracker.currentQual % 3;
+        switch (currentNormalizedQaul)
+        {
+            case 0:
+                qualityDisplayText.text = low;
+                break;
+            case 1:
+                qualityDisplayText.text = medium;
+                break;
+            case 2:
+                qualityDisplayText.text = high;
+                break;
+            default:
+                break;
+        }
+        print(qualityDisplayText.text);
     }
 }
